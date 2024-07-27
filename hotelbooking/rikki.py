d706c056e6522dfff52dfc488ae2ebf37b52f443
sivasanthosh-363 <siva363santhosh@gmail.com>
2024-07-27 16:26:03 +0530

    Add files via upload

---
+++ hotelbooking/rikki.py
@@ -0,0 +1,97 @@
+import json
+
+def validate(slots):
+    if not slots.get('Location'):
+        print("Inside Empty Location")
+        return {
+            'isValid': False,
+            'violatedSlot': 'Location'
+        }
+        
+    if not slots.get('CheckInDate'):
+        return {
+            'isValid': False,
+            'violatedSlot': 'CheckInDate'
+        }
+        
+    if not slots.get('Nights'):
+        return {
+            'isValid': False,
+            'violatedSlot': 'Nights'
+        }
+        
+    if not slots.get('RoomType'):
+        return {
+            'isValid': False,
+            'violatedSlot': 'RoomType'
+        }
+
+    return {'isValid': True}
+
+def lambda_handler(event, context):
+    
+    slots = event['sessionState']['intent']['slots']
+    intent = event['sessionState']['intent']['name']
+    print(event['invocationSource'])
+    print(slots)
+    print(intent) 
+    
+    validation_result = validate(slots)
+    
+    if event['invocationSource'] == 'DialogCodeHook':
+        if not validation_result['isValid']:
+    
+            response = {
+                "sessionState": {
+                    "dialogAction": {
+                        'slotToElicit': validation_result['violatedSlot'],
+                        "type": "ElicitSlot"
+                    },
+                    "intent": {
+                        'name':intent,
+                        'slots': slots
+                        
+                        }
+                }
+               }
+        else:
+            response = {
+            "sessionState": {
+                "dialogAction": {
+                    "type": "Delegate"
+                },
+                "intent": {
+                    'name':intent,
+                    'slots': slots
+                    
+                    }
+        
+            }
+        }
+    if event['invocationSource'] == 'FulfillmentCodeHook':
+        
+        # Add order in Database
+        
+        response = {
+        "sessionState": {
+            "dialogAction": {
+                "type": "Close"
+            },
+            "intent": {
+                'name':intent,
+                'slots': slots,
+                'state':'Fulfilled'
+                
+                }
+    
+        },
+        "messages": [
+            {
+                "contentType": "PlainText",
+                "content": "Thanks, I have placed your reservation"
+            }
+        ]
+    }
+    return response
+
+  
